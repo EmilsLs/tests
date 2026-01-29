333f644323df7bf78e783c23d2e5a6c49af6e522
EmilsLs <15.e.lacis-sejans@cesunovads.edu.lv>
2026-01-29 09:04:42 +0000

    v01

---
+++ Dalisana veselos skaitlos/pirmais.py
@@ -0,0 +1,4 @@
+nauda=int(input())
+eiro=nauda//100
+centi=nauda%100
+print("Ir "+str(eiro)+" eiro  un "+str(centi)+" centi.")
---
+++ Dalisana veselos skaitlos/tresais.py
@@ -0,0 +1,10 @@
+h1=int(input())
+h2=int(input())
+m1=int(input())
+m2=int(input())
+s1=int(input())
+s2=int(input())
+h3=h2-h1
+m3=m2-m1
+s3=s2-s1
+print ("Sacensību rezultāti ir: \n"+str(h3)+" Stundas\n"+str(m3)+" Minūtes\n"+str(s3)+" Sekundes" )
